919ce221836c80cf6e9189b28c3769120e2d3d01
Anna C <annac25@uw.edu>
2017-03-03 01:03:07 -0800

    added more trees -ac

--- skiing/sketch.js
+++ skiing/sketch.js
@@ -1,6 +1,8 @@
-jumpValue = -200 //Y value of George's jump
+jumpValueR = -200
+jumpValueS = 0 //Y value of George's jump
 jumpDir = 0 //speed and direction
 r = 0;
+s = 5;
 
 function preload() {
   table = loadTable("tree_arrays.csv", "csv", "header");
@@ -14,7 +16,8 @@ function setup() {
 function draw() {
   background(255);
   currentTime = time();
-  jumpValue = jumpValue - jumpDir;
+  jumpValueR = jumpValueR - jumpDir;
+  jumpValueS = jumpValueS - jumpDir;
   fill(0); //make text white
   text("X: " + mouseX, 0, 5, 300, 300); // print x coordinate in upper left corner
   text("Y: " + mouseY, 0, 20, 300, 300); // print y coordinate in upper left corner, under x coordinate 
@@ -24,31 +27,65 @@ function draw() {
   var levelOne = rows[r].getNum("Level-1");
   var levelTwo = rows[r].getNum("Level-2");
   var levelThree = rows[r].getNum("Level-3");
+  var levelFour = rows[s].getNum("Level-4");
+  var levelFive = rows[s].getNum("Level-5");
+  var levelSix = rows[s].getNum("Level-6");
 
-  tree(levelOne, 700 + jumpValue);
-  tree(levelTwo, 700 + jumpValue);
-  tree(levelThree, 700 + jumpValue);
+  if (1 < currentTime || currentTime < 450) {
+    tree(levelOne, 700 + jumpValueR);
+    tree(levelTwo, 750 + jumpValueR);
+    tree(levelThree, 800 + jumpValueR);
+    tree(levelFour, 1000 + jumpValueR);
+    tree(levelFive, 1050 + jumpValueR);
+    tree(levelSix, 1100 + jumpValueR);
+    
+  }
+
+  if (100 < currentTime || currentTime < 490 ) {
+    tree(levelOne, 700 + jumpValueS);
+    tree(levelTwo, 750 + jumpValueS);
+    tree(levelThree, 750 + jumpValueS);
+    tree(levelFour, 800 + jumpValueS);
+    tree(levelFive, 900 + jumpValueS);
+    tree(levelSix, 1000 + jumpValueS);
+  }
+
+
+
 
   // tree(100, 600 + jumpValue)
-  if (currentTime > 10) { //if frame rate is greater than 800, make the balloons stop moving 
-    //balloonX moves off the screen
-    jumpDir = 2; //balloonY movement
+  if (currentTime > 1) { //if frame rate is greater than 10, begin tree movement 
+    //causes trees to scroll up the screen
+    jumpDir = 2; //tree movement
   }
   if (r < rows.length) {
-    if (currentTime == 300) {
+    if (currentTime == 495) {
       r++;
     }
   }
-  
-  if (currentTime > 300) {
-    jumpValue = -200;
-    jumpValue = jumpValue - jumpDir;
-/*    tree(100, 600 + jumpValue)*/
+
+  if (s < rows.length) {
+    if (currentTime == 50) {
+      s++;
+    }
   }
+
+
+  if (currentTime > 498) {
+    jumpValueR = -200;
+    jumpValueR = jumpValueR - jumpDir;
+    /*    tree(100, 600 + jumpValue)*/
+  }
+
+  if (currentTime > 498) {
+    jumpValueS = 0;
+    jumpValueS = jumpValueS - jumpDir;
+  }
+
 }
 
 function time() {
-  return frameCount % 310; // modulo loops frameCount at 1800 by returning remainder after dividing by 1800
+  return frameCount % 500; // modulo loops frameCount at 1800 by returning remainder after dividing by 1800
 }
 
 function tree(treeX, treeY) {
